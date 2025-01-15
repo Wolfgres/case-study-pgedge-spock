5186e65e932f84c47c972987948dee78c2dc3f58
lmartinez <llibran.martinez@gmail.com>
2025-01-15 12:37:55 -0600

    Se actualiza la sintaxis de el script 00_init.sql

--- postgres_replication/00_init.sql
+++ postgres_replication/00_init.sql
@@ -1,4 +1,3 @@
-create user replicator with replication encrypted password 'replicator_password';
-create user wolfgres_user password 'password';
-create database wolfgres_db;
-select pg_create_physical_replication_slot('streaming_standby_server');
+CREATE USER replicator WITH REPLICATION ENCRYPTED PASSWORD '0UHl3Q14MnFL';
+CREATE USER wolfgres_user PASSWORD 'K7E0Tj2y1qlq';
+SELECT pg_create_physical_replication_slot('streaming_standby_server');
