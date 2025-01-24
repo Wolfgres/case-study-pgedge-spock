fbdc699b038264fead1222724a084981b2cd0bd7
lmartinez <llibran.martinez@gmail.com>
2025-01-24 14:19:49 -0600

    Correction in sequence name operation

--- wolfgres_db/sequences_to_snowflake.sql
+++ wolfgres_db/sequences_to_snowflake.sql
@@ -10,5 +10,5 @@
 ALTER TABLE wfg.account ALTER COLUMN account_id SET DEFAULT snowflake.nextval('wfg.account_account_id_seq');
 ALTER TABLE wfg.account_type ALTER COLUMN account_type_id SET DEFAULT snowflake.nextval('wfg.account_type_account_type_id_seq');
 ALTER TABLE wfg.customer ALTER COLUMN customer_id SET DEFAULT snowflake.nextval('wfg.customer_customer_id_seq');
-ALTER TABLE wfg.operation ALTER COLUMN operation_id SET DEFAULT snowflake.nextval('wfg.peration_operation_id_seq');
+ALTER TABLE wfg.operation ALTER COLUMN operation_id SET DEFAULT snowflake.nextval('wfg.operation_operation_id_seq');
 ALTER TABLE wfg.transaction ALTER COLUMN transaction_id SET DEFAULT snowflake.nextval('wfg.transaction_transaction_id_seq');
