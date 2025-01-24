69581de23736dcbd0578503c995e287b4140b4e9
lmartinez <llibran.martinez@gmail.com>
2025-01-24 14:17:02 -0600

    Schema added to sequences_to_snowflake.sql script

--- wolfgres_db/sequences_to_snowflake.sql
+++ wolfgres_db/sequences_to_snowflake.sql
@@ -7,8 +7,8 @@
 --ALTER TABLE wfg.transaction ALTER COLUMN transaction_id TYPE bigint;
 
 -- ALTER TABLE
-ALTER TABLE wfg.account ALTER COLUMN account_id SET DEFAULT snowflake.nextval('account_account_id_seq');
-ALTER TABLE wfg.account_type ALTER COLUMN account_type_id SET DEFAULT snowflake.nextval('account_type_account_type_id_seq');
---ALTER TABLE wfg.customer ALTER COLUMN customer_id SET DEFAULT snowflake.nextval();
-ALTER TABLE wfg.operation ALTER COLUMN operation_id SET DEFAULT snowflake.nextval('operation_operation_id_seq');
-ALTER TABLE wfg.transaction ALTER COLUMN transaction_id SET DEFAULT snowflake.nextval('transaction_transaction_id_seq');
+ALTER TABLE wfg.account ALTER COLUMN account_id SET DEFAULT snowflake.nextval('wfg.account_account_id_seq');
+ALTER TABLE wfg.account_type ALTER COLUMN account_type_id SET DEFAULT snowflake.nextval('wfg.account_type_account_type_id_seq');
+ALTER TABLE wfg.customer ALTER COLUMN customer_id SET DEFAULT snowflake.nextval('wfg.customer_customer_id_seq');
+ALTER TABLE wfg.operation ALTER COLUMN operation_id SET DEFAULT snowflake.nextval('wfg.peration_operation_id_seq');
+ALTER TABLE wfg.transaction ALTER COLUMN transaction_id SET DEFAULT snowflake.nextval('wfg.transaction_transaction_id_seq');
